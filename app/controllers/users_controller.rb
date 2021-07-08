116bb46a0f6b5bd7077dd5d754349e9756ecdcd2
yusuke ohta <youohta7755@gmail.com>
2021-07-08 16:21:32 +0900

    権限編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email,:password, :password_confirmation, :admin)
+    params.require(:user).permit(:name, :email,:password, :password_confirmation)
   end
 
   def set_user
